c5d133f5e98bbf6d39fe7252b18d4dc3db988fd5
Egor1705 <BsuHelp@tut.by>
2022-08-04 17:59:38 +0300

    ternary

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
+        int max = 0;
+        System.out.println(first>second?"first":"second");
     }
 }
